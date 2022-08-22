5fed0a2861a6845eaee864cfe2c0c4c478678c95
Björn Paulsen <paulsen@bewidata.de>
2022-08-22 13:38:22 +0200

    inital

---
+++ src/main/java/com/example/mainloadersbpclean/MainLoaderSbpCleanApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainloadersbpclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainLoaderSbpCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainLoaderSbpCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mainloadersbpclean/MainLoaderSbpCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mainloadersbpclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainLoaderSbpCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
